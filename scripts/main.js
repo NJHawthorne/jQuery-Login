bb4ac19f68eb0ba205d821f5fe2ce228ac39018f
Nate Hawthorne <natejhawthorne@gmail.com>
2016-03-25 21:51:52 -0500

    Mostly finished product. Still not sure how to change the URL to another location through pushing a button and no idea how to make it work.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,102 @@
 //Setting up a constructor for my two objects
-function UserLogin (username, password) {
-	this.username = username;
+function UserLogin (email, password) {
+	this.email = email;
 	this.password = password;
 }
-
-var usernameInput = $('.usernameInput');
-var passwordInput = $('.passwordInput');
 
 //Creating both objects for aaron's login and admin's login
 var aaron = new UserLogin('aaron@theironyard.com', 'password123');
 var admin = new UserLogin('admin@google.com', 'pandas');
 
-//Making sure everything worked with the constructor
-console.log(aaron);
-console.log(admin);
+function manageAaronEmail() {
+	if(!$('.emailInput').val() || !$('.emailInput').val().split('').includes('@')) {
+		$('.emailError').html('Please enter a valid email address');
+		return false;
+	} else if ($('.emailInput').val() === aaron.email) {
+		$('.emailError').html('');
+		return true;
+	} else {
+		$('.emailError').html('Your email address was not correct. Please try again');
+		return false;
+	}
+};
 
-//Starting the event handler to verify information
-$('.loginBtn').on('submit', function() {
+function manageAdminEmail() {
+	if(!$('.emailInput').val() || !$('.emailInput').val().split('').includes('@')) {
+		$('.emailError').html('Please enter a valid email address');
+		return false;
+	} else if ($('.emailInput').val() === admin.email) {
+		$('.emailError').html('');
+		return true;
+	} else {
+		$('.emailError').html('Your email address was not correct. Please try again');
+		return false;
+	}
+};
 
+function manageAaronPassword() {
+	if(!$('.passwordInput').val()) {
+		$('.passwordError').html('Please enter a valid password');
+		return false;
+	} else if ($('.passwordInput').val() === aaron.password) {
+		$('.passwordError').html('');
+		return true;
+	} else {
+		$('.passwordError').html('Your password was not correct. Please try again');
+		return false;
+	}
+};
+
+function manageAdminPassword() {
+	if(!$('.passwordInput').val()) {
+		$('.passwordError').html('Please enter a valid password');
+		return false;
+	} else if ($('.passwordInput').val() === admin.password) {
+		$('.passwordError').html('');
+		return true;
+	} else {
+		$('.passwordError').html('Your password was not correct. Please try again');
+		return false;
+	}
+};
+
+$('form').submit(function(e) {
+	e.preventDefault;
+	var adminEmailTrue = manageAdminEmail();
+	var aaronEmailTrue = manageAaronEmail();
+	var adminPasswordTrue = manageAdminPassword();
+	var aaronPasswordTrue = manageAaronPassword();
+
+	if(adminPasswordTrue && adminEmailTrue) {
+		console.log('It worked');
+		window.location = 'https://theironyard.com';
+	} else if (aaronEmailTrue && aaronPasswordTrue) {
+		console.log('It worked');
+		window.location = 'https://theironyard.com';
+	}
 });
 
-function redirect () {
-	window.location.replace("http://www.facebook.com");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
